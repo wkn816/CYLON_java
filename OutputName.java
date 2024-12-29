d44d078eb61a5297958bd94b6e6503095fbeadf5
wkn816 <wkn816@gmail.com>
2024-12-30 03:54:22 +0900

    first commit

---
+++ OutputName.java
@@ -0,0 +1,7 @@
+class OutputName {
+  public static void main(String[] args){
+      String name;
+      name = "田中太郎";
+      System.out.println(name);
+  }
+}
---
+++ OutputValue.java
@@ -0,0 +1,15 @@
+class OutputValue {
+  public static void main(String[] args){
+      final int VALUE = 100;
+      System.out.println(VALUE);
+
+      String VarA;
+      int VarB = 100;
+      final int CONST_A = 200;
+
+      VarA = "テスト";
+      System.out.println(VarA);
+      System.out.println(VarB);
+      System.out.println(CONST_A);
+  }
+}
