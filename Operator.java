aae50e6a4874aff3df14c1b28f72dc9fc0c4158f
wkn816 <wkn816@gmail.com>
2024-12-30 18:11:03 +0900

    3-5.演算子

---
+++ Operator.java
@@ -0,0 +1,15 @@
+class Operator {
+
+  // メゾット
+  public static void main(String[] args) {
+    int a = 2;
+    int b = 6;
+    System.out.println( a + b );
+    System.out.println( a - b );
+    System.out.println( a * b );
+    System.out.println( a / b );
+    System.out.println( a % b );
+    System.out.println( b = --a );
+    System.out.println( a += 2 );
+  }
+}
